7117e2be0edd1be483e19a9bbe7ef0f226fc1647
XxFri3ndlyxX <18711345+XxFri3ndlyxX@users.noreply.github.com>
2019-04-29 18:46:55 -0400

    Feature Added
    ```
    1. Added Cooldown on chopping vehicles.
    2. Added NPC at shop location with config to disable it and change coordination.
    3. Vehicle now gets door removed after opening them. Raised time per door to 5000ms  so 5 sec. 
    4. Added the bility to hide blips in config.
    5. Reworked the way you did the chopping. So removed from key control and added his own function So ChopVehicle.
    6. Renamed old ChopVehicle to DeleteVehicle
    ```

--- config.lua
+++ config.lua
@@ -1,16 +1,22 @@
 Config = {}
 
 Config.DrawDistance               = 100.0
+Config.EnableBlips                = true -- Set to false to disable blips
 Config.MarkerType                 = 27
 Config.MarkerColor                = { r = 50, g = 50, b = 204 }
 
 Config.Locale                     = 'en'
+Config.CooldownMinutes            = 1 -- Minutes between chopping.
 
 
-Config.GiveBlack = true -- Wanna use Blackmoney?
+Config.NPCEnable                  = true -- Set to false to disable NPC Ped at shop location.
+Config.NPCHash					  = 68070371 --Hash of the npc ped. Change only if you know what you are doing.
+Config.NPCShop	                  = { x = -55.42, y = 6392.8, z = 30.5, h = 46.0 } -- Location of the shop For the npc
+
+Config.GiveBlack                  = true -- Wanna use Blackmoney?
 
 Config.Zones = {
-	Chopshop = {coords = vector3(-531.34, -1713.45, 19.20), name = _U('map_blip'), color = 49, sprite = 225, radius = 100.0, Pos = { x = -531.34, y = -1713.45, z = 18.3}, Size  = { x = 5.0, y = 5.0, z = 0.5 }, },
+	Chopshop = {coords = vector3(-522.87, -1713.99, 18.33), name = _U('map_blip'), color = 49, sprite = 225, radius = 100.0, Pos = { x = -522.87, y = -1713.99, z = 18.33}, Size  = { x = 5.0, y = 5.0, z = 0.5 }, },
 	Shop = {coords = vector3(-55.42, 6392.8, 30.5), name = _U('map_blip_shop'), color = 50, sprite = 120, radius = 25.0, Pos = { x = -55.42, y = 6392.8, z = 30.5}, Size  = { x = 3.0, y = 3.0, z = 1.0 }, },
 
 }
