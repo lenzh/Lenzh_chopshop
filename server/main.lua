9ed26c6213ca8e1caa28afb4b3989d0a99d1847c
XxFri3ndlyxX <18711345+XxFri3ndlyxX@users.noreply.github.com>
2019-04-29 18:46:15 -0400

    Features Added
    ```
    1. Added Cooldown on chopping vehicles.
    2. Added NPC at shop location with config to disable it and change coordination.
    3. Vehicle now gets door removed after opening them. Raised time per door to 5000ms  so 5 sec. 
    4. Added the bility to hide blips in config.
    5. Reworked the way you did the chopping. So removed from key control and added his own function So ChopVehicle.
    6. Renamed old ChopVehicle to DeleteVehicle
    ```

--- server/main.lua
+++ server/main.lua
@@ -1,21 +1,26 @@
 ESX = nil
-
+local cooldown = 0
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+ESX.RegisterServerCallback('Lenzh_chopshop:isCooldown',function(source, cb)
+	cb(cooldown)
+  end)
 
 RegisterServerEvent("lenzh_chopshop:rewards")
 AddEventHandler("lenzh_chopshop:rewards", function()
   Rewards()
+  --Add cooldown
+	cooldown = Config.CooldownMinutes * 60000
 end)
 
 function Rewards(rewards)
   local xPlayer = ESX.GetPlayerFromId(source)
   	if not xPlayer then return; end
-
     for k,v in pairs(Config.Items) do
   		local randomCount = math.random(0, 3)
   		xPlayer.addInventoryItem(v, randomCount)
-  	end
+	  end
 end
 
 RegisterServerEvent('lenzh_chopshop:sell')
@@ -41,4 +46,14 @@ AddEventHandler('lenzh_chopshop:sell', function(itemName, amount)
 	xPlayer.removeInventoryItem(xItem.name, amount)
 
 	TriggerClientEvent('esx:showNotification', source, _U('sold', amount, xItem.label, ESX.Math.GroupDigits(price)))
+end)
+
+--Cooldown manager
+AddEventHandler('onResourceStart', function(resource)
+	while true do
+		Wait(5000)
+		if cooldown > 0 then
+			cooldown = cooldown - 5000
+		end
+	end
 end)
