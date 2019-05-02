43b632e4a582e0bc067a9ded3a2a6aecf44ebcc0
Lenzh <lorenzo0109@hotmail.nl>
2019-05-02 02:57:35 +0200

    Set Config.EnableBlips = true

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 Config = {} -- DON'T TOUCH
 
 Config.DrawDistance       = 100.0 -- Change the distance before you can see the marker. Less is better performance.
-Config.EnableBlips        = false -- Set to false to disable blips.
+Config.EnableBlips        = true -- Set to false to disable blips.
 Config.MarkerType         = 27    -- Change to -1 to disable marker.
 Config.MarkerColor        = { r = 50, g = 50, b = 204 } -- Change the marker color.
 
