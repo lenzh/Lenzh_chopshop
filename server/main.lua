4fa419dbd569c3017143bf59c1efa5a856d4c7f4
Lenzh <lorenzo0109@hotmail.nl>
2019-06-25 23:36:09 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,36 +1,44 @@
+
+
 ESX = nil
 
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-ESX.RegisterServerCallback('Lenzh_chopshop:anycops',function(source, cb)
-  local anycops = 0
-  local playerList = GetPlayers()
-  for i=1, #playerList, 1 do
-    local _source = playerList[i]
-    local xPlayer = ESX.GetPlayerFromId(_source)
-    local playerjob = xPlayer.job.name
-    if playerjob == 'police' then
-      anycops = anycops + 1
-    end
-  end
-  cb(anycops)
-end)
+if GetCurrentResourceName() == 'Lenzh_chopshop' then
 
-RegisterServerEvent("lenzh_chopshop:rewards")
-AddEventHandler("lenzh_chopshop:rewards", function(rewards)
-    --Rewards()
-    local _source = source
-	local xPlayer = ESX.GetPlayerFromId(_source)
-    if not xPlayer then return; end
-    for k,v in pairs(Config.Items) do
-        local randomCount = math.random(0, 3)
-        xPlayer.addInventoryItem(v, randomCount)
-    end
+  print("\n###############################")
+  print("\n".. GetCurrentResourceName() .. " Loaded ")
+  print("\n###############################")
 
-end)
+    ESX.RegisterServerCallback('Lenzh_chopshop:anycops',function(source, cb)
+        local anycops = 0
+        local playerList = GetPlayers()
+        for i=1, #playerList, 1 do
+            local _source = playerList[i]
+            local xPlayer = ESX.GetPlayerFromId(_source)
+            local playerjob = xPlayer.job.name
+            if playerjob == 'police' then
+                anycops = anycops + 1
+            end
+        end
+        cb(anycops)
+    end)
 
---[[ function Rewards(rewards)
+    RegisterServerEvent("lenzh_chopshop:rewards")
+    AddEventHandler("lenzh_chopshop:rewards", function(rewards)
+        --Rewards()
+        local _source = source
+        local xPlayer = ESX.GetPlayerFromId(_source)
+        if not xPlayer then return; end
+        for k,v in pairs(Config.Items) do
+            local randomCount = math.random(0, 3)
+            xPlayer.addInventoryItem(v, randomCount)
+        end
+
+    end)
+
+    --[[ function Rewards(rewards)
     local xPlayer = ESX.GetPlayerFromId(source)
     if not xPlayer then return; end
     for k,v in pairs(Config.Items) do
@@ -39,51 +47,57 @@ end)
     end
 end ]]
 
-RegisterServerEvent('chopNotify')
-AddEventHandler('chopNotify', function()
-    TriggerClientEvent("chopEnable", source)
-end)
+    RegisterServerEvent('chopNotify')
+    AddEventHandler('chopNotify', function()
+        TriggerClientEvent("chopEnable", source)
+    end)
 
 
-RegisterServerEvent('ChopInProgress')
-AddEventHandler('ChopInProgress', function(street1, street2, sex)
-    TriggerClientEvent("outlawChopNotify", -1, "")
-end)
+    RegisterServerEvent('ChopInProgress')
+    AddEventHandler('ChopInProgress', function(street1, street2, sex)
+        TriggerClientEvent("outlawChopNotify", -1, "")
+    end)
 
 
-RegisterServerEvent('ChopInProgressS1')
-AddEventHandler('ChopInProgressS1', function(street1, sex)
-    TriggerClientEvent("outlawChopNotify", -1, "")
+    RegisterServerEvent('ChopInProgressS1')
+    AddEventHandler('ChopInProgressS1', function(street1, sex)
+        TriggerClientEvent("outlawChopNotify", -1, "")
 
-end)
+    end)
 
-RegisterServerEvent('ChoppingInProgressPos')
-AddEventHandler('ChoppingInProgressPos', function(gx, gy, gz)
-    TriggerClientEvent('Choplocation', -1, gx, gy, gz)
-end)
-
-
-RegisterServerEvent('lenzh_chopshop:sell')
-AddEventHandler('lenzh_chopshop:sell', function(itemName, amount)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local price = Config.Itemsprice[itemName]
-    local xItem = xPlayer.getInventoryItem(itemName)
+    RegisterServerEvent('ChoppingInProgressPos')
+    AddEventHandler('ChoppingInProgressPos', function(gx, gy, gz)
+        TriggerClientEvent('Choplocation', -1, gx, gy, gz)
+    end)
 
 
-    if xItem.count < amount then
-        TriggerClientEvent('esx:showNotification', source, _U('not_enough'))
-        return
-    end
+    RegisterServerEvent('lenzh_chopshop:sell')
+    AddEventHandler('lenzh_chopshop:sell', function(itemName, amount)
+        local xPlayer = ESX.GetPlayerFromId(source)
+        local price = Config.Itemsprice[itemName]
+        local xItem = xPlayer.getInventoryItem(itemName)
 
-    price = ESX.Math.Round(price * amount)
 
-    if Config.GiveBlack then
-        xPlayer.addAccountMoney('black_money', price)
-    else
-        xPlayer.addMoney(price)
-    end
+        if xItem.count < amount then
+            TriggerClientEvent('esx:showNotification', source, _U('not_enough'))
+            return
+        end
 
-    xPlayer.removeInventoryItem(xItem.name, amount)
+        price = ESX.Math.Round(price * amount)
 
-    TriggerClientEvent('esx:showNotification', source, _U('sold', amount, xItem.label, ESX.Math.GroupDigits(price)))
-end)
+        if Config.GiveBlack then
+            xPlayer.addAccountMoney('black_money', price)
+        else
+            xPlayer.addMoney(price)
+        end
+
+        xPlayer.removeInventoryItem(xItem.name, amount)
+
+        TriggerClientEvent('esx:showNotification', source, _U('sold', amount, xItem.label, ESX.Math.GroupDigits(price)))
+    end)
+else
+    print("\n###############################")
+    print("\n DO NOT RENAME MY CHOPSHOP FUCKERS... Change it ".. GetCurrentResourceName() .. " should be:\n".. 'Lenzh_chopshop')
+    print("\n###############################")
+
+end
